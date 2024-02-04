af548307128bd8a7e9cb3c4e7724431958015bdf
Aditya Banerjee <aditya_banerjee@icloud.com>
2024-02-04 15:41:17 +0800

    add logging

--- telegram-bot/src/components/telegram.ts
+++ telegram-bot/src/components/telegram.ts
@@ -1,13 +1,17 @@
+import { type Logger as Log4js } from 'log4js';
 import Bot, { type Message } from 'node-telegram-bot-api';
+import { Logger } from 'components/logger';
 
 const { TELEGRAM_BOT_TOKEN: token = '', NODE_ENV: env = 'DEV' } = process.env;
 
 export class Telegram {
     private static instance: Telegram;
     protected bot: Bot;
+    private logger: Log4js;
 
     protected constructor() {
         this.bot = new Bot(token, { polling: env === 'DEV' });
+        this.logger = Logger.getLogger('Telegram');
     }
 
     static getInstance(): Telegram {
@@ -18,6 +22,7 @@ export class Telegram {
     }
 
     initialize(): void {
+        this.logger.info('initialising bot');
         this.bot.on('message', (message: Message) => {
             this.processMessage(message);
         });
@@ -25,6 +30,7 @@ export class Telegram {
 
     async processMessage(message: Message): Promise<void> {
         const { text, chat } = message;
+        this.logger.info(`processing message ${text}`);
         const { id: chatId } = chat;
         this.bot.sendMessage(chatId, `Received message ${text}`);
     }
