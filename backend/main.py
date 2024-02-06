687f89bbb4ec0cf8284fd41d807b777e7f0af27b
Vishesh Arora <>
2024-02-06 12:17:03 +0800

    Add API Key

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,30 @@
+import os
+from dotenv import load_dotenv
 from datetime import datetime
 from typing import Any, Dict, List, Literal, Optional
 import chromadb
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import Depends, FastAPI, HTTPException, Query, Security, status
+from fastapi.security import APIKeyHeader
 from pydantic import BaseModel, Field
 from custom_logger import setup_custom_logger
 
+load_dotenv()
+
+
 # Initialize FastAPI app
 app = FastAPI()
+
+
+api_key = os.environ.get('API_KEY')
+api_key_header = APIKeyHeader(name="X-API-Key")
+
+def get_api_key(api_key_header: str = Security(api_key_header)) -> str:
+    if api_key_header == api_key:
+        return api_key_header
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid or missing API Key",
+    )
 
 client = chromadb.HttpClient(host="localhost", port = 8000)
 collection = client.get_or_create_collection(name="messages")
@@ -29,7 +47,7 @@ class MessageInfo(BaseModel):
 
 
 @app.post("/messages/", response_model=List[MessageInfo])
-async def upsert_messages(messages: List[MessageInfo]):
+async def upsert_messages(messages: List[MessageInfo], api_key: str = Security(get_api_key)):
 
     ids = [msg.message_id for msg in messages]
     documents = [msg.message_content for msg in messages]
@@ -42,14 +60,14 @@ async def upsert_messages(messages: List[MessageInfo]):
 
 
 @app.get("/message/{message_id}") # response_model=TelegramMessage)
-async def get_message_by_id(message_id: str):
+async def get_message_by_id(message_id: str, api_key: str = Security(get_api_key)):
     message = collection.get(message_id)
     return message
 
 
 
 @app.get("/messages/search/") # TODO - Add response model
-async def search_messages(search_string: str = Query(..., min_length=1)):
+async def search_messages(search_string: str = Query(..., min_length=1), api_key: str = Security(get_api_key)):
     query_text = f"Who says this - {search_string}"
 
     results = collection.query(
@@ -63,12 +81,12 @@ async def search_messages(search_string: str = Query(..., min_length=1)):
 
 
 @app.delete("/messages/delete/", response_model=List[str])
-async def delete_messages(message_ids: List[str]):
+async def delete_messages(message_ids: List[str], api_key: str = Security(get_api_key)):
     collection.delete(ids=message_ids)
     return message_ids
 
 
 @app.delete("/messages/delete_all/")
-async def delete_messages():
+async def delete_messages(api_key: str = Security(get_api_key)):
     client.delete_collection("messages")
     return {"Deleted": "RESTART API NOW!!"}
