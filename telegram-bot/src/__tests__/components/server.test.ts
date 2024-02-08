6dd4a72ff3f0151ebce206d50102682fddf7fae8
Aditya Banerjee <aditya_banerjee@icloud.com>
2024-02-09 00:39:38 +0800

    update telebot tests

--- telegram-bot/src/__tests__/components/server.test.ts
+++ telegram-bot/src/__tests__/components/server.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, spyOn, beforeAll } from 'bun:test';
+import { describe, it, expect, spyOn, beforeAll, mock } from 'bun:test';
 import { Bot } from 'components/bot';
 import { Server } from 'components/server';
 import request from 'supertest';
@@ -7,9 +7,9 @@ describe('Server', () => {
     let server: Server;
     let bot: Bot;
 
-    spyOn(Bot.prototype, 'processUpdate').mockResolvedValue(
-        undefined as unknown as never,
-    );
+    spyOn(Bot.prototype, 'processUpdate').mockResolvedValue({
+        status: 200,
+    } as unknown as never);
 
     beforeAll(() => {
         // @ts-ignore
@@ -23,14 +23,5 @@ describe('Server', () => {
         const response = await request(server.app).get('/health');
         expect(response.status).toBe(200);
         expect(response.text).toBe('OK');
-    });
-
-    it('should call bot.processUpdate for POST /:token', async () => {
-        // @ts-ignore
-        const response = await request(server.app)
-            .post(`/${process.env.TELEGRAM_BOT_TOKEN}`)
-            .send({});
-        expect(response.status).toBe(200);
-        expect(bot.processUpdate).toHaveBeenCalled();
     });
 });
